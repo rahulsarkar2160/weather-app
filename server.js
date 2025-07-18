73089423edbff6c60374b00d0c7b4ca885933d70
rahulsarkar2160 <rahulsarkar2160@gmail.com>
2025-07-18 16:17:58 +0530

    Switch to relative static path for public folder

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 3000;
 // Setup views and static folder
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
